0c3665a3087826481227a9974c7e095433329568
Jose Pereira <josebatistapereira@gmail.com>
2025-10-03 07:22:01 -0300

    First commit, init backend project

---
+++ backend/chirp/src/test/kotlin/io/github/josebatista/chirp/ChirpApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.josebatista.chirp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChirpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
