1b6a6d3146f6e31d4a58fea825d5639c738d43db
Jose Pereira <josebatistapereira@gmail.com>
2026-02-08 19:09:03 -0300

    Device Configuration For Responsive UIs

--- Chirp/core/presentation/build.gradle.kts
+++ Chirp/core/presentation/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
 
                 implementation(projects.core.domain)
-
+                implementation(libs.material3.adaptive)
                 implementation(compose.components.resources)
             }
         }
