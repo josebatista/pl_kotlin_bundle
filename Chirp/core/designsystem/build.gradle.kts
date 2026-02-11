5942861b174111ffcf0d56006117ebeeae6e0895
Jose Pereira <josebatistapereira@gmail.com>
2026-02-11 08:14:15 -0300

    Core buttons

--- Chirp/core/designsystem/build.gradle.kts
+++ Chirp/core/designsystem/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
                 // Add KMP dependencies here
                 implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
             }
         }
 
